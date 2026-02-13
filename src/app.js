edf9bda8f1dd3ae87871f0d817903780d9c4153c
ClearlyOpaque <hariprasadaji465@gmail.com>
2026-02-13 23:51:06 +0530

    final modifications..

--- src/app.js
+++ src/app.js
@@ -12,8 +12,8 @@ class Gallery extends React.Component {
   images = [
     require('../1.jpg'),
     require('../2.jpg'),
-    // require('../3.jpg'),
-    // require('../4.jpg'),
+    require('../3.jpg'),
+    require('../4.jpg'),
     require('../5.jpg'),
 
   ];
